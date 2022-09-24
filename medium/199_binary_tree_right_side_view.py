b038a73afa5c1119ff56c61b0370ebf2855e4ea8
thinhnw <thinhnv0412@gmail.com>
2022-09-24 23:21:55 +0700

    Solve problem #199

---
+++ medium/199_binary_tree_right_side_view.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+      
+    def dfs(self, node, level):
+        if not node:
+            return
+        if level >= len(self.ans):
+            self.ans.append([])
+        self.ans[level].append(node.val)
+        if node.left:
+            self.dfs(node.left, level + 1)
+        if node.right:
+            self.dfs(node.right, level + 1)
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        self.ans = []
+        self.dfs(root, 0)
+        for l in self.ans:
+            yield l[-1]
