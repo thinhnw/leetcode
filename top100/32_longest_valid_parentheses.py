12fab9db9002b919428712f46f3c41ab179bdd82
thinhnw <thinhnv0412@gmail.com>
2022-10-02 12:36:33 +0700

    Optimize #32 a little bit, ACCEPTED

--- top100/32_longest_valid_parentheses.py
+++ top100/32_longest_valid_parentheses.py
@@ -1,13 +1,24 @@
 class Solution:
-    # def calc(self, i, k):
-        
-    #     if s[i] == '(':
-    #         if self.calc(i - 1, k - 1) != -1:
-    #             self.f[i][k] =  self.calc(i - 1, k - 1) + 1
-    #         else:
-    #             self.f[i][k] = -1
-    #     else:
-    #         if self.calc()
+    def calc(self, i, k):
+        if i == -1:
+            return 0 if not k else -1
+        if k in self.f[i]:
+            return self.f[i][k]
+        if self.s[i] == '(':
+            if k == 0:
+                self.f[i][k] = 0
+            elif self.calc(i - 1, k - 1) != -1:
+                self.f[i][k] =  self.calc(i - 1, k - 1) + 1
+            else:
+                self.f[i][k] = -1
+        else:
+            if self.calc(i - 1, k + 1) != -1:
+                self.f[i][k] = self.calc(i - 1, k + 1) + 1
+            elif k:
+                self.f[i][k] = -1
+            elif not k:
+                self.f[i][k] = 0
+        return self.f[i][k]
     def longestValidParentheses(self, s: str) -> int:
         # f[i][k]: longest length if there is a substring that ends at i and have k open brackets else 0
         # f[i][k] >= k
@@ -17,27 +28,14 @@ class Solution:
             return 0
         n = len(s)
         res = 0
-        f = [-1 for _ in range((n >> 1) + 1)]
-        f[0] = 0
+        self.s = s
+        self.f = [{} for _ in range(n)]
         for i in range(n):
-            if s[i] == '(':
-                for k in reversed(range(1,(n >> 1) + 1)):
-                    if f[k - 1] != -1:
-                        f[k] = f[k - 1] + 1
-                    else:
-                        f[k] = -1
-                f[0] = 0
-            else:
-                for k in range(1, (n>>1) + 1):
-                    if f[k] != -1:
-                        f[k - 1] = f[k] + 1
-                    elif k - 1:
-                        f[k - 1] = -1
-                    else:
-                        f[0] = 0
-            # print(f)
-            res = max(res, f[0])
+            res = max(res, self.calc(i, 0))
+        # for i in range(n):
+        #     print(self.f[i])
+        # print(self.calc(0, 1))
         return res
 
-Solution().longestValidParentheses(")()())()()(")
+print(Solution().longestValidParentheses(")()())()()("))
         
