b43a5e4a2e9ab02a4324cf0e407cd80fd6093a4c
thinhnw <thinhnv0412@gmail.com>
2024-08-21 12:01:15 +0700

    Solve #374 (Binary Search)

---
+++ binary-search/374_guess_number_higher_or_lower.py
@@ -0,0 +1,25 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 1
+        high = n
+        while low < high:
+            mid = (high + low) >> 1
+            g = guess(mid)
+            if g == 0:
+                return mid
+            elif g == -1:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
